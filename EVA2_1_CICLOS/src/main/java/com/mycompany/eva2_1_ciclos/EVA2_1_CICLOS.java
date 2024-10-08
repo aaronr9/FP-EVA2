71bac5df251157534e9f27dd08c74eb5fdf314ef
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-10-08 16:55:22 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS/src/main/java/com/mycompany/eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_1_ciclos;
+
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_1_CICLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //REPETICION DE INTRUCCIONES:
+        //CICLOS O BUCLES
+        //CICLO FOR(desde)
+        
+        for(int i = 1; i <= 10; i++){
+            //++--> operador para sumar 1 unidad
+            //-- --> resta una unidad
+            //+= suma N valor
+            //-= resta N valor
+            //*=Multiplicar por N
+            // /= divide entre N
+            System.out.print(i+"-");
+           }
+        System.out.println("");
+        for(int i = 10; i >= 1; i--){
+            System.out.print(i+"-");
+        }
+        
+       
+                
+    }          
+}
