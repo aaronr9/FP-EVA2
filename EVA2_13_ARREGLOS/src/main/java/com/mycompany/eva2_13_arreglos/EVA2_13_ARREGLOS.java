c9bb30f703e84cde7384a0e935f805f8279f187e
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-11-05 20:21:34 -0600

    Add files via upload

---
+++ EVA2_13_ARREGLOS/src/main/java/com/mycompany/eva2_13_arreglos/EVA2_13_ARREGLOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_13_arreglos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_13_ARREGLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //int[]--> indica que es unarreglo de enteros
+        //new int[10] --> crea el arreglo con 10 espacios para enteros
+        int[] arreglo = new int[10];
+        arreglo[0] = 100;//ponemos 100 en el entero en la posicion cero
+        arreglo[9]= 900; //última posición [10 - 1]
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
+        System.out.println(arreglo[6]);
+        System.out.println(arreglo[7]);
+        System.out.println(arreglo[8]);
+        System.out.println(arreglo[9]);
+       
+        
+        
+    }
+}
