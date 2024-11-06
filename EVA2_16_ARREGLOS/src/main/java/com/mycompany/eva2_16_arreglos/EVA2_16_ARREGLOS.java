8b8337567374233f38c9abbbe2c4d3bfd70465d2
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-11-05 20:22:46 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS/src/main/java/com/mycompany/eva2_16_arreglos/EVA2_16_ARREGLOS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_arreglos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_16_ARREGLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String[] diasSemana = {"Domingo, Lunes, Martes, Miercoles, Jueves, Viernes, Sabado"};
+         //IMPRIMIR
+         for (int i = 0; i < diasSemana.length; i++) {
+             System.out.println(diasSemana [i]);
+            
+        }
+        
+        
+    }
+}
