7c9a37e02a33f25131e844ac45735c48e5a3d395
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-11-05 20:21:51 -0600

    Add files via upload

---
+++ EVA2_14_ARREGLOS/src/main/java/com/mycompany/eva2_14_arreglos/EVA2_14_ARREGLOS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_14_arreglos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_14_ARREGLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+        
+    }
+}
