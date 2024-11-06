b8964a96c8b417bce767468095d368543fd6f44c
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-11-05 20:20:34 -0600

    Add files via upload

---
+++ EVA2_12_TIPOS_DE_DATOS/src/main/java/com/mycompany/eva2_12_tipos_de_datos/EVA2_12_TIPOS_DE_DATOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_12_tipos_de_datos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_12_TIPOS_DE_DATOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        byte valor = -128;
+        byte valor2 = 127;
+        System.out.println("Valor = " + (valor));
+        System.out.println("Valor2 = " + (valor2));
+        short val = -32768;
+        short val2 = 32767;
+        int x = -2147483618;
+        int y = 2147483617;
+        
+        
+    }
+}
